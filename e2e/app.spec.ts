91ab1dbb5a756c880cef5aaef365bf05274c9083
nicholasnet <nirmalp@hotmail.com>
2023-10-15 22:31:32 -0400

    Reverted E2E test.

--- e2e/app.spec.ts
+++ e2e/app.spec.ts
@@ -5,8 +5,5 @@ import { test, expect } from '@playwright/test'
 test('visits the app root url and show dependency dialog', async ({ page }) => {
     await page.goto('/')
     await expect(page.locator('body')).toHaveCount(1)
-    await expect(page.locator('body > div')).toHaveCount(0)
-    await expect(page.getByRole('button', { name: 'Add Dependencies' })).toHaveCount(1)
-    await page.getByRole('button', { name: 'Add Dependencies' }).click()
-    await expect(page.locator('body > div')).toHaveCount(1)
+    // await expect(page.locator('body > div')).toHaveCount(0)
 })
